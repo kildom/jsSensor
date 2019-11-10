7eff8bc21ab3927f0eed66b27e8f2b31e492b648
kildom <kildom@users.noreply.github.com>
2019-11-10 22:47:35 +0100

    Add Ref class unit tests and fix Ref class issues

--- app/ref.h
+++ app/ref.h
@@ -1,21 +1,20 @@
 #ifndef _REF_H_
 #define _REF_H_
 
+#include <cstddef>
+
 template<class T>
 class Ref
 {
 private:
-    T* ptr;
 
-    void inc() {
+    static void inc(T* ptr) {
+        if (!ptr) return;
         ((uint32_t*)ptr)[-1]++;
     }
 
-    void dec() {
-        ((uint32_t*)ptr)[-1]--;
-    }
-
-    void decAndCheck() {
+    static void dec(T* ptr) {
+        if (!ptr) return;
         uint32_t c = --((uint32_t*)ptr)[-1];
         if (c == 0)
         {
@@ -26,13 +25,33 @@ private:
     Ref(T* data) { ptr = data; }
 
 public:
+
+    T* ptr;
+
     Ref() { ptr = nullptr; }
-    Ref(const Ref<T>& old) { ptr = old.ptr; if (ptr) inc(); }
-    Ref(Ref<T>&& old) { ptr = old.ptr; old.ptr = nullptr; }
-    ~Ref() { decAndCheck(); }
+    Ref(const Ref& old) { ptr = old.ptr; inc(ptr); }
+    Ref(Ref&& old) { ptr = old.ptr; old.ptr = nullptr; }
+    Ref(std::nullptr_t) { ptr = nullptr; }
+    ~Ref() { dec(ptr); }
+
+    Ref& operator=(const Ref& old) { inc(old.ptr); dec(ptr); ptr = old.ptr; return *this; }
+    Ref& operator=(Ref&& old) { dec(ptr); ptr = old.ptr; old.ptr = nullptr; return *this; }
+    Ref& operator=(std::nullptr_t) { dec(ptr); ptr = nullptr; return *this; }
+
+    T* operator->() { return ptr; }
+    T& operator*() { return *ptr; }
+
+    bool operator==(const Ref& other) const { return ptr == other.ptr; }
+    bool operator!=(const Ref& other) const { return ptr != other.ptr; }
+    bool operator==(std::nullptr_t) const { return ptr == nullptr; }
+    bool operator!=(std::nullptr_t) const { return ptr != nullptr; }
+    friend bool operator==(std::nullptr_t, const Ref<T>& other) { return nullptr == other.ptr; }
+    friend bool operator!=(std::nullptr_t, const Ref<T>& other) { return nullptr != other.ptr; }
+
+    operator bool() { return ptr != nullptr; }
     
     template <typename ...Params>
-    static Ref<T> make(Params&&... params)
+    static Ref make(Params&&... params)
     {
         struct CntAndData
         {
@@ -43,7 +62,8 @@ public:
         CntAndData* ptr = new CntAndData(std::forward<Params>(params)...);
         return Ref(&ptr->data);
     }
-
+    
 };
+
 
 #endif // _REF_H_
---
+++ test/test_ref.cc
@@ -0,0 +1,162 @@
+#include <set>
+#include <string>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "ref.h"
+
+struct TestClsMocks
+{
+    MOCK_METHOD(void, constructor, ());
+    MOCK_METHOD(void, constructor, (float, std::string));
+    MOCK_METHOD(void, constructorWithPF, ());
+    MOCK_METHOD(void, destructor, (void*));
+
+    MOCK_METHOD(void, copyConstructor, ());
+    MOCK_METHOD(void, moveConstructor, ());
+    MOCK_METHOD(void, copyAssign, ());
+    MOCK_METHOD(void, moveAssign, ());
+    MOCK_METHOD(void, end, ());
+} testClsMocks;
+
+struct TestPF
+{
+    TestPF() {}
+    TestPF(const TestPF&) { testClsMocks.copyConstructor(); }
+    TestPF(TestPF&&) { testClsMocks.moveConstructor(); }
+    TestPF& operator=(const TestPF&) { testClsMocks.copyAssign(); return *this; }
+    TestPF& operator=(TestPF&&) { testClsMocks.moveAssign(); return *this; }
+};
+
+struct TestEnd
+{
+    ~TestEnd() { testClsMocks.end(); }
+};
+
+struct TestCls
+{
+    static std::set<TestCls*> instances;
+    TestCls() { instances.insert(this); testClsMocks.constructor(); }
+    TestCls(float a, std::string b) { instances.insert(this); testClsMocks.constructor(a, b); }
+    TestCls(const TestPF&) { testClsMocks.constructorWithPF(); }
+    TestCls(int, TestPF) { testClsMocks.constructorWithPF(); }
+    ~TestCls() { testClsMocks.destructor(this); instances.erase(this); }
+};
+
+std::set<TestCls*> TestCls::instances;
+
+TEST(Ref, CreateDelete) {
+    {
+        testing::InSequence seq;
+        EXPECT_CALL(testClsMocks, constructor());
+        Ref<TestCls> ref = Ref<TestCls>::make();
+        EXPECT_CALL(testClsMocks, destructor(testing::_));
+    }
+    {
+        testing::InSequence seq;
+        EXPECT_CALL(testClsMocks, constructor(1.2f, "test"));
+        Ref<TestCls> ref = Ref<TestCls>::make(1.2f, "test");
+        EXPECT_CALL(testClsMocks, destructor(testing::_));
+    }
+    {
+        testing::InSequence seq;
+        EXPECT_CALL(testClsMocks, constructorWithPF());
+        Ref<TestCls> ref = Ref<TestCls>::make(TestPF());
+        EXPECT_CALL(testClsMocks, destructor(testing::_));
+    }
+    {
+        testing::InSequence seq;
+        EXPECT_CALL(testClsMocks, moveConstructor());
+        EXPECT_CALL(testClsMocks, constructorWithPF());
+        Ref<TestCls> ref = Ref<TestCls>::make(1, TestPF());
+        EXPECT_CALL(testClsMocks, destructor(testing::_));
+    }
+}
+
+
+TEST(Ref, CopyAssign) {
+    testing::InSequence seq;
+    TestEnd end;
+    EXPECT_CALL(testClsMocks, constructor())
+        .Times(2);
+    Ref<TestCls> a = Ref<TestCls>::make();
+    Ref<TestCls> b = Ref<TestCls>::make();
+    TestCls* pa = a.ptr;
+    TestCls* pb = b.ptr;
+    EXPECT_CALL(testClsMocks, destructor(pa));
+    a = b;
+    EXPECT_EQ(((uint32_t*)pb)[-1], 2);
+    EXPECT_CALL(testClsMocks, destructor(pb));
+    EXPECT_CALL(testClsMocks, end());
+}
+
+
+TEST(Ref, MoveAssign) {
+    testing::InSequence seq;
+    TestEnd end;
+    EXPECT_CALL(testClsMocks, constructor());
+    Ref<TestCls> a = Ref<TestCls>::make();
+    TestCls* pa = a.ptr;
+    EXPECT_CALL(testClsMocks, constructor());
+    EXPECT_CALL(testClsMocks, destructor(pa));
+    a = Ref<TestCls>::make();
+    pa = a.ptr;
+    EXPECT_EQ(((uint32_t*)pa)[-1], 1);
+    EXPECT_CALL(testClsMocks, destructor(pa));
+    EXPECT_CALL(testClsMocks, end());
+}
+
+
+TEST(Ref, CopyConstr) {
+    testing::InSequence seq;
+    TestEnd end;
+    EXPECT_CALL(testClsMocks, constructor());
+    Ref<TestCls> b = Ref<TestCls>::make();
+    Ref<TestCls> a(b);
+    TestCls* pb = b.ptr;
+    EXPECT_EQ(((uint32_t*)pb)[-1], 2);
+    b = nullptr;
+    EXPECT_EQ(((uint32_t*)pb)[-1], 1);
+    EXPECT_CALL(testClsMocks, destructor(pb));
+    a = nullptr;
+    EXPECT_CALL(testClsMocks, end());
+}
+
+
+TEST(Ref, MoveConstr) {
+    testing::InSequence seq;
+    TestEnd end;
+    EXPECT_CALL(testClsMocks, constructor());
+    Ref<TestCls> a(Ref<TestCls>::make());
+    TestCls* pa = a.ptr;
+    EXPECT_EQ(((uint32_t*)pa)[-1], 1);
+    EXPECT_CALL(testClsMocks, destructor(pa));
+    a = nullptr;
+    EXPECT_CALL(testClsMocks, end());
+}
+
+TEST(Ref, Compare) {
+    EXPECT_CALL(testClsMocks, constructor())
+        .Times(2);
+    EXPECT_CALL(testClsMocks, destructor(testing::_))
+        .Times(2);
+    Ref<TestCls> a = Ref<TestCls>::make();
+    Ref<TestCls> b = Ref<TestCls>::make();
+    Ref<TestCls> c = a;
+    EXPECT_TRUE(a == c);
+    EXPECT_FALSE(b == c);
+    EXPECT_TRUE(b != c);
+    EXPECT_FALSE(a != c);
+    c = nullptr;
+    EXPECT_FALSE(a == c);
+    EXPECT_TRUE(a != c);
+    EXPECT_FALSE(a == nullptr);
+    EXPECT_TRUE(a != nullptr);
+    EXPECT_TRUE(c == nullptr);
+    EXPECT_FALSE(nullptr != c);
+    EXPECT_FALSE(nullptr == a);
+    EXPECT_TRUE(nullptr != a);
+    EXPECT_TRUE(nullptr == c);
+    EXPECT_FALSE(nullptr != c);
+}
